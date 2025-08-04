0a9761d5568d70192a1c48f3868bb15ad4c0480b
Mateus Couto <mateuscouto98@outlook.com>
2025-08-04 03:17:46 -0300

    created a module and service AI

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuestionsModule } from './questions/questions.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { GeminiModule } from './gemini/gemini.module';
 
 @Module({
-  imports: [QuestionsModule, PrismaModule],
+  imports: [QuestionsModule, PrismaModule, GeminiModule],
   controllers: [AppController],
   providers: [AppService],
 })
